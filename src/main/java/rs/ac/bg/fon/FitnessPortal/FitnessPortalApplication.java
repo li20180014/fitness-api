15798a09f44bb293dee8cb938faf32b3a97081da
li20180014 <li20180014@student.fon.bg.ac.rs>
2022-08-09 19:29:40 +0200

    Initialize project
    Setup Maven project with dependecies. Create folder structure

---
+++ src/main/java/rs/ac/bg/fon/FitnessPortal/FitnessPortalApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.FitnessPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessPortalApplication.class, args);
+	}
+
+}
