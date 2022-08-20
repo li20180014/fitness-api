de0a404e509bae12417a6a0e5382e856c280c26d
li20180014 <li20180014@student.fon.bg.ac.rs>
2022-08-20 19:28:39 +0200

    Generate Javadoc for UserProfileInformation entity

--- src/main/java/rs/ac/bg/fon/FitnessPortal/entities/UserProfileInformation.java
+++ src/main/java/rs/ac/bg/fon/FitnessPortal/entities/UserProfileInformation.java
@@ -2,31 +2,83 @@ package rs.ac.bg.fon.FitnessPortal.entities;
 
 import javax.persistence.*;
 
+/**
+ * Class representing the application User's personal profile information.
+ *
+ * Contains personal information about Users who have created their account and wish to log their height, weight, age and gender.
+ * Class attributes: unique id, height, weight, age, gender and User with whom this profile information is associated with.
+ *
+ * @author Lana
+ * @version 1.0
+ */
 @Entity(name = "users_profile_information")
 public class UserProfileInformation {
 
+    /**
+     * Unique id number representing the primary key in database table. Id is a primary generated auto increment value.
+     */
     @Id
     @GeneratedValue
     private Integer id;
+
+    /**
+     * User's height, default value is 0.
+     */
     private Integer height;
 
+    /**
+     * User's weight, default value is 0.
+     */
     private Integer weight;
+
+    /**
+     * User's age, default value is 0.
+     */
     private Integer age;
+
+    /**
+     * User's gender, default value is 0.
+     */
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    /**
+     * User's associated with profile information, default value is null.
+     */
     @OneToOne(optional = false)
     @JoinColumn(name = "user_id")
     private User user;
 
+    /**
+     * Class constructor, sets attributes to their default values.
+     */
     public UserProfileInformation() { }
 
+    /**
+     * Sets attributes height, weight, age and gender to entered values. Sets other attributes to their default values.
+     *
+     * @param height
+     * @param weight
+     * @param age
+     * @param gender
+     */
     public UserProfileInformation(Integer height, Integer weight, Integer age, Gender gender) {
         this.height = height;
         this.weight = weight;
         this.age = age;
         this.gender = gender;
     }
+
+    /**
+     * Sets attributes height, weight, age, gender and User to entered values.
+     *
+     * @param id
+     * @param height
+     * @param weight
+     * @param age
+     * @param gender
+     * @param user
+     */
     public UserProfileInformation(Integer id, Integer height, Integer weight, Integer age, Gender gender, User user) {
         this.id = id;
         this.height = height;
@@ -36,51 +88,111 @@ public class UserProfileInformation {
         this.user = user;
     }
 
+    /**
+     * Returns the user profile id number.
+     *
+     * @return id as an integer.
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Sets the user profile id to entered value.
+     *
+     * @param id new integer value for user profile id.
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Returns the user associated with profile.
+     *
+     * @return user as an object of class User.
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Sets the user associated with profile to entered value.
+     *
+     * @param user new User entity associated with profile.
+     */
     public void setUser(User user) {
         this.user = user;
         if(user != null) user.setUserProfileInformation(this);
     }
 
+    /**
+     * Returns the user height.
+     *
+     * @return height as an integer.
+     */
     public Integer getHeight() {
         return height;
     }
 
+    /**
+     * Sets users height to entered value.
+     *
+     * @param height new integer value for users height.
+     */
     public void setHeight(Integer height) {
         this.height = height;
     }
 
+    /**
+     * Returns the user weight.
+     *
+     * @return weight as an integer.
+     */
     public Integer getWeight() {
         return weight;
     }
 
+    /**
+     * Sets users weight to entered value.
+     *
+     * @param weight new integer value for users weight.
+     */
     public void setWeight(Integer weight) {
         this.weight = weight;
     }
 
+    /**
+     * Returns the users age.
+     *
+     * @return age as an integer.
+     */
     public Integer getAge() {
         return age;
     }
 
+    /**
+     * Sets users age to entered value.
+     *
+     * @param age new integer value for users age.
+     */
     public void setAge(Integer age) {
         this.age = age;
     }
 
+    /**
+     * Returns the users gender .
+     *
+     * @return gender as a value of Enum type Gender.
+     */
     public Gender getGender() {
         return gender;
     }
 
+    /**
+     * Sets users gender to entered value.
+     *
+     * @param gender new enum value of type Gender for users gender.
+     */
     public void setGender(Gender gender) {
         this.gender = gender;
     }
