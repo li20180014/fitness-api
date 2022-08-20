4337a803607f4a8186de86382edf16cfa7178b69
li20180014 <li20180014@student.fon.bg.ac.rs>
2022-08-20 19:27:40 +0200

    Generate Javadoc for Training and Appointment entities

--- src/main/java/rs/ac/bg/fon/FitnessPortal/entities/Appointment.java
+++ src/main/java/rs/ac/bg/fon/FitnessPortal/entities/Appointment.java
@@ -2,59 +2,129 @@ package rs.ac.bg.fon.FitnessPortal.entities;
 
 import javax.persistence.*;
 
+/**
+ * Class representing an appointment as a scheduled training session.
+ *
+ * Contains information about the member who has scheduled a training session provided by the clubs coach.
+ * Class attributes: unique id, member, training, attended.
+ *
+ * @author Lana
+ * @version 1.0
+ */
 @Entity(name = "appointments")
 public class Appointment {
 
+    /**
+     * Unique id number representing the primary key in database table. Id is a primary generated auto increment value.
+     */
     @Id
     @GeneratedValue
     @Column(name = "appointment_id")
     private Integer id;
 
+    /**
+     * Member who has made the appointment - scheduled the training session.
+     */
     @ManyToOne
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
+    /**
+     * The training session provided by the clubs coach.
+     */
     @ManyToOne
     @JoinColumn(name = "training_id", nullable = false)
     private Training training;
 
+    /**
+     * Record of the member's attendance on the scheduled appointment.
+     */
     private Boolean attended;
 
+    /**
+     * Class constructor, sets attributes to their default values.
+     */
     public Appointment() { }
 
+    /**
+     * Sets attributes member and training to entered values.
+     *
+     * @param member
+     * @param training
+     */
     public Appointment(Member member, Training training) {
         this.member = member;
         this.training = training;
     }
 
+    /**
+     * Returns the id number of the scheduled appointment.
+     *
+     * @return id as an integer value.
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Sets appointment id to entered value.
+     *
+     * @param id new integer value as id.
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Returns the member who has scheduled the training session.
+     *
+     * @return member as an object of class Member.
+     */
     public Member getMember() {
         return member;
     }
 
+    /**
+     * Sets the member of the appointment to entered value.
+     *
+     * @param member new object of class Member as member.
+     */
     public void setMember(Member member) {
         this.member = member;
     }
 
+    /**
+     * Returns the scheduled training session.
+     *
+     * @return training as an object of class Training.
+     */
     public Training getTraining() {
         return training;
     }
 
+    /**
+     * Sets the appointments training session to entered value.
+     *
+     * @param training new object of class Training as the appointments training.
+     */
     public void setTraining(Training training) {
         this.training = training;
     }
 
+    /**
+     * Returns information about the member attendance on the appointment.
+     *
+     * @return attended as a boolean value.
+     */
     public Boolean getAttended() {
         return attended;
     }
 
+    /**
+     * Sets attendance to entered value.
+     *
+     * @param attended new boolean value as attendance.
+     */
     public void setAttended(Boolean attended) {
         this.attended = attended;
     }
--- src/main/java/rs/ac/bg/fon/FitnessPortal/entities/Training.java
+++ src/main/java/rs/ac/bg/fon/FitnessPortal/entities/Training.java
@@ -5,97 +5,213 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Set;
 
+/**
+ * Class representing a training session provided by a fitness coach.
+ *
+ * Contains information about duration of training, number of members who can attend and the coach in charge.
+ * Class attributes: unique id, date, startTime, endTime, maxSpots, remainingSpots, coach and .
+ *
+ * @author Lana
+ * @version 1.0
+ */
 @Entity(name = "trainings")
 public class Training {
 
+    /**
+     * Unique id number representing the primary key in database table. Id is a primary generated auto increment value.
+     */
     @Id
     @GeneratedValue
     private Integer id;
 
+    /**
+     * Date of the training session.
+     */
     @Column(nullable = false)
     private LocalDate date;
 
+    /**
+     * Start time of the training session.
+     */
     @Column(nullable = false)
     private LocalTime startTime;
 
+    /**
+     * End time of the training session.
+     */
     @Column(nullable = false)
     private LocalTime endTime;
 
+    /**
+     * Maximum number of members who can attend the training session.
+     */
     @Column(nullable = false)
     private Integer maxSpots;
 
+    /**
+     * Remaining spots for training session.
+     */
     @Column(nullable = false)
     private Integer remainingSpots;
 
+    /**
+     * Coach in charge of training session.
+     */
     @ManyToOne
     @JoinColumn(name = "coach_id", nullable = false)
     private Coach coach;
 
+    /**
+     * Representing the scheduled training session by a member.
+     */
     @OneToMany(mappedBy = "training", cascade = CascadeType.ALL)
     private Set<Appointment> appointments;
 
+    /**
+     * Class constructor, sets attributes to their default values.
+     */
     public Training() { }
 
+    /**
+     * Returns the training id.
+     *
+     * @return id as an integer value.
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Sets the id to entered value.
+     *
+     * @param id new integer value as id.
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Returns the date the training is being held.
+     *
+     * @return date as LocalDate value.
+     */
     public LocalDate getDate() {
         return date;
     }
 
+    /**
+     * Sets the date to entered value.
+     *
+     * @param date new LocalDate value as date.
+     */
     public void setDate(LocalDate date) {
         this.date = date;
     }
 
+    /**
+     * Returns the time the training starts.
+     *
+     * @return startTime as LocalTime value.
+     */
     public LocalTime getStartTime() {
         return startTime;
     }
 
+    /**
+     * Sets startTime to entered value.
+     *
+     * @param startTime  new LocalTime value as start time.
+     */
     public void setStartTime(LocalTime startTime) {
         this.startTime = startTime;
     }
 
+    /**
+     * Returns the time the training ends.
+     *
+     * @return endTime as LocalTime value.
+     */
     public LocalTime getEndTime() {
         return endTime;
     }
 
+    /**
+     * Sets endTime to entered value.
+     *
+     * @param endTime new LocalTime value as endTime.
+     */
     public void setEndTime(LocalTime endTime) {
         this.endTime = endTime;
     }
 
+    /**
+     * Returns the coach in charge of the training session.
+     *
+     * @return coach as an object of class Coach.
+     */
     public Coach getCoach() {
         return coach;
     }
 
+    /**
+     * Sets coach to entered value.
+     *
+     * @param coach new object of class Coach.
+     */
     public void setCoach(Coach coach) {
         this.coach = coach;
     }
 
+    /**
+     * Returns maximum number of spots for training session.
+     *
+     * @return maxSpots as integer value.
+     */
     public Integer getMaxSpots() {
         return maxSpots;
     }
 
+    /**
+     * Sets max number of spots to entered value.
+     *
+     * @param maxSpots new integer value as max number of spots.
+     */
     public void setMaxSpots(Integer maxSpots) {
         this.maxSpots = maxSpots;
     }
 
+    /**
+     * Returns the remaining  number of spots for training session.
+     *
+     * @return remainingSpots as integer value.
+     */
     public Integer getRemainingSpots() {
         return remainingSpots;
     }
 
+    /**
+     * Sets remaining spots to entered value.
+     *
+     * @param remainingSpots new integer value as number of remaining spots.
+     */
     public void setRemainingSpots(Integer remainingSpots) {
         this.remainingSpots = remainingSpots;
     }
-    
+
+    /**
+     * Returns the list of scheduled appointments for selected training session.
+     *
+     * @return appointments as set of objects of class Appointment.
+     */
     public Set<Appointment> getAppointments() {
         return appointments;
     }
 
+    /**
+     * Sets appointments to entered values.
+     *
+     * @param appointments new set of objects of class Appointment.
+     */
     public void setAppointments(Set<Appointment> appointments) {
         this.appointments = appointments;
     }
